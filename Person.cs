14ea2dad6cfbdc1b22b48874a63e08fae4e37e29
TranMaiAnh710 <maianh070705@gmail.com>
2026-01-28 13:57:02 +0700

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Abstraction
+{
+    public abstract class Person
+    {
+        protected String? ID;
+        protected String? HoTen;
+        protected String? SDT;
+
+        public abstract void HienthiTTin();
+    }
+}
